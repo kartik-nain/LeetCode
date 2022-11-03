f7ac021585df1e78db4d281379b7b530bfda2c9b
kartik-nain <109487621+kartik-nain@users.noreply.github.com>
2022-11-03 21:17:49 +0530

    Time: 3 ms (26.93%), Space: 42.8 MB (50.34%) - LeetHub

---
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int titleToNumber(String columnTitle) {
+        int sum = 0;
+
+        for (char c : columnTitle.toUpperCase().toCharArray()) {
+            sum *= 26;
+            sum += c - 'A' + 1;
+        }
+        
+        return sum;
+    }
+}
