cd514fce8ba67a1472c2e4843ac94a031c102140
kartik-nain <109487621+kartik-nain@users.noreply.github.com>
2022-11-09 13:08:09 +0530

    Time: 0 ms (100.00%), Space: 54.9 MB (7.76%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -6,8 +6,8 @@ class Solution {
 		while(left<=right) {
             int mid = left + (right-left)/2; //To avoid integer overflow
             
-            if(arr[mid] == val){
-                return mid;
+            if(arr[mid] == val){  //Return mid only when 1 value is giving result. When 2 values give same result and we have to choose between 2, return left or right based on what is asked(See Koko eating bananas)
+                return mid; 
             }else if(val<arr[mid]) {
 				right = mid-1;
 			}else {
