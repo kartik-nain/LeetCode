41c60c1f6e4ddfbe689ce5456008c16c50dedae5
kartik-nain <109487621+kartik-nain@users.noreply.github.com>
2022-11-11 23:54:41 +0530

    Time: 5 ms (70.67%), Space: 43.8 MB (52.69%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,28 +1,23 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        List<Integer> ans = new ArrayList<>();
-        int i1 =0;
-        int i2 = 0;
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        ArrayList<Integer> ans = new ArrayList<>();
         
-        while(i1<nums1.length && i2<nums2.length){
-            if(nums1[i1] == nums2[i2]){
-                ans.add(nums1[i1]);
-                i1++;
-                i2++;
-            }else if(nums1[i1]<nums2[i2]){
-                i1++;
-            }else{
-                i2++;
+        for(int num: nums1){
+            hm.put(num, hm.getOrDefault(num, 0)+1);
+        }
+        
+        for(int num: nums2){
+            if(hm.containsKey(num) && hm.get(num)!=0){
+                ans.add(num);
+                hm.put(num, hm.get(num)-1);
             }
         }
         
-        int[] arr = new int[ans.size()];
+        int[] a = new int[ans.size()];
         for(int i = 0; i<ans.size(); i++){
-            arr[i] = ans.get(i);
+            a[i] = ans.get(i);
         }
-        
-        return arr;
+        return a;
     }
 }
