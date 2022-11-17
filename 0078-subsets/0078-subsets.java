ae44159d143290c29cf22aef716f5ad3801962a8
kartik-nain <109487621+kartik-nain@users.noreply.github.com>
2022-11-17 00:24:20 -0800

    Time: 1 ms (87.84%), Space: 42.9 MB (77.37%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -7,14 +7,18 @@ class Solution {
     }
     
     public void backTrack(List<List<Integer>> ans, List<Integer> temp, int[] nums, int index){
+        
+        //Base condition
         if(index == nums.length){
             ans.add(new ArrayList<>(temp)); //Can't add temp directly because we need a deep copy or a current snapshot of the temp list as this list keeps on changing. Only shallow here will add references to the list
             return;
         }
         
+        //Pick the element and move ahead
         temp.add(nums[index]);
         backTrack(ans, temp, nums, index+1);
         
+        //Don't pick the element and move ahead
         temp.remove(temp.size()-1);
         backTrack(ans, temp, nums, index+1);
     }
